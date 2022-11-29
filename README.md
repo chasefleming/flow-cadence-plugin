00a9f7a99c0eb38bf49013f4be213ee3cc0700ba
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2022-11-29 14:29:49 -0800

    Initial creation of the plugin

---
+++ README.md
@@ -0,0 +1,31 @@
+This is a webpack plugin for watching and deploying Cadence files on the Flow blockchain.
+
+## Adding the Plugin
+
+### Webpack
+
+```
+new FlowCadencePlugin({ 
+  network: process.env.FLOW_NETWORK,
+  deployOnChange: true,
+  update: true 
+}
+```
+
+### Next.js
+
+```
+const FlowCadencePlugin = require('flow-cadence-plugin')
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(new FlowCadencePlugin({ 
+      network: process.env.FLOW_NETWORK,
+      deployOnChange: true,
+      update: true 
+    }))
+
+    return config;
+  },
+}
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+const util = require('util')
+const {exec} = require('child_process')
+const execAsync = util.promisify(exec)
+
+const cleanNetwork = network => network?.toLowerCase() === 'local' ? 'emulator' : network?.toLowerCase()
+
+class FlowCadencePlugin {
+  constructor({ network, deployOnChange, update }) {
+    this.network = network
+    this.deployOnChange = deployOnChange
+    this.update = update
+  }
+  
+  apply(compiler) {
+    compiler.options.module.rules.push({
+      test: /\.cdc$/i,
+      loader: 'raw-loader',
+    })
+
+    compiler.hooks.afterEmit.tapAsync(
+      {
+       name: 'FlowCadencePlugin',
+      },
+      async (compilation, callback) => {
+        let command = `flow project deploy --network=${cleanNetwork(this.network)}`
+
+        if (this.update) {
+          command = `${command} --update`
+        }
+
+        if (this.deployOnChange) {
+          await execAsync(`flow project deploy --network=${cleanNetwork(this.network)} --update`)
+        }
+    
+        callback()
+      }
+     )
+  }
+}
+
+module.exports = FlowCadencePlugin
